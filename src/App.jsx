d11ea937d519e9cb6125f6c0eed8234a7fe0dca4
isaac-svg <isaacsakyi009@gmail.com>
2023-05-23 19:19:10 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = process.env.API_URL;
+  axios.defaults.baseURL ="https://mern-chatapp-api.vercel.app/";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
